3a2db23fa8becdce018fd14f4b10f2348772cb77
Jessie S. Zamora <jzamss@gmail.com>
2022-06-22 13:29:56 +0800

    util scripts

---
+++ scripts/misc.sql
@@ -0,0 +1,13 @@
+/* check invalid suffix */
+select * from h_property_info 
+where prop_type_code <> 'L' 
+and pin_no not regexp '.*[0-9][0-9][0-9][0-9]$'
+
+set @pin_no:='047-46-0002-010-30-(1001)                       ';
+
+select right(replace(replace(trim(@pin_no),'(', ''), ')', ''), 4);
+
+
+
+/* check invalid section */
+select section from h_property_info order by section desc limit 5;
